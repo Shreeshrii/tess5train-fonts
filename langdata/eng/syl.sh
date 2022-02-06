57ecde77d5f85ede219ef1fc3d7dea0e7b03fd57
Shree Devi Kumar <5095331+Shreeshrii@users.noreply.github.com>
2022-02-06 02:58:32 +0000

    Update langdata

--- langdata/eng/syl.sh
+++ langdata/eng/syl.sh
@@ -12,7 +12,7 @@ rm -rf tmp*
 awk '{if (f==1) { r[$0] } else if (! ($0 in r)) { print $0 } } ' f=1 $BASE-eval.training_text f=2 $BASE.txt > tmp-X.txt
 
 cat $BASE.syl | while IFS="
-" read target; do grep -F -m 50 "$target"  tmp-X.txt ; done > tmp2.txt
+" read target; do grep -F -m 100 "$target"  tmp-X.txt ; done > tmp2.txt
 awk '{if (f==1) { r[$0] } else if (! ($0 in r)) { print $0 } } ' f=1 tmp2.txt f=2 tmp-X.txt > $BASE-REMAIN.txt
 sort -u tmp2.txt   > tmp4.txt
 create_dictdata -i  tmp4.txt -d ./ -l $BASE-train
@@ -22,4 +22,4 @@ rm -rf tmp*
 python3 ../../count_chars.py $BASE-train.training_text | sort -n -r > $BASE-train.count_chars.txt
 
 rm *bigram*
-#rm *.wordlist
+rm *.wordlist
